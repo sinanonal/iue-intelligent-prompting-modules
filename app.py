999da91fb27c15bc4693ba6acdaa327c5640b35b
sinanonal <78244877+sinanonal@users.noreply.github.com>
2026-01-21 14:33:07 -0600

    app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ if page == "🎧 Lecture Video / Voice Recording":
     # PDF NOTES
     st.markdown("### 📄 Downloadable PDF Notes")
     try:
-        with open("notes/module1_notes.pdf", "rb") as pdf_file:
+        with open("notes/SETTIN~1.PDF", "rb") as pdf_file:
             st.download_button(
                 label="Download Module 1 Notes",
                 data=pdf_file,
