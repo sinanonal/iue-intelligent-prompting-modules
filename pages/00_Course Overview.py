b53b9a59ac613901489c1c50b7eff57b9dd5fed5
sinanonal <78244877+sinanonal@users.noreply.github.com>
2026-01-22 13:00:26 -0600

    Update 00_Course Overview.py

--- pages/00_Course Overview.py
+++ pages/00_Course Overview.py
@@ -252,4 +252,4 @@ Write a short reflection (about **100–150 words**):
         overview_download_block()
 
     st.markdown("---")
-    st.page_link("pages/01_Module_1.py", label="➡️ Go to Module 1")
+    st.page_link("pages/01_Module 1.py", label="➡️ Go to Module 1")
